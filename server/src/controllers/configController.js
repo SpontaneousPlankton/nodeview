58bed5e07761284abcf862f7d99e9e4cf5c02cde
JeremyIR <jeremy.i.rosenfeld@gmail.com>
2016-05-17 14:48:24 -0700

    update models and controllers

--- server/src/controllers/configController.js
+++ server/src/controllers/configController.js
@@ -1,11 +1,20 @@
 import Config from '../models/config.js';
+import User from '../models/user.js';
 
 export function createOne(request, response) {
-  const newConfig = new Config({});
+  const newConfig = new Config({
+    user: user._id,
+    'data.serverType': request.body.data.serverType,
+    'data.appName': request.body.data.appName,
+    'data.serverSettings.port': request.body.data.serverSettings.port,
+    'data.routers': request.body.data.routers,
+  });
+
   newConfig.save((err) => {
     if (err) {
       return response.status(500).json(err);
     }
-    return response.json(newConfig);
+    return response.send(newConfig);
   });
 }
+
