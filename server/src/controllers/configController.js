49165c32fc911ece62dcf65898966047a1166cf5
JeremyIR <jeremy.i.rosenfeld@gmail.com>
2016-05-17 11:09:23 -0700

    remove double quotes

--- server/src/controllers/configController.js
+++ server/src/controllers/configController.js
@@ -4,10 +4,10 @@ import User from '../models/user.js';
 export function createOne(request, response) {
   const newConfig = new Config({
     // user:
-    "data.serverType": request.body.data.serverType,
-    "data.appName": request.body.data.appName,
-    "data.serverSettings.port": request.body.data.serverSettings.port,
-    "data.routers": request.body.data.routers,
+    'data.serverType': request.body.data.serverType,
+    'data.appName': request.body.data.appName,
+    'data.serverSettings.port': request.body.data.serverSettings.port,
+    'data.routers': request.body.data.routers,
   });
 
   newConfig.save((err) => {
@@ -17,6 +17,3 @@ export function createOne(request, response) {
     return response.send(newConfig);
   });
 }
-
-
-
