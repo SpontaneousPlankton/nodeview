6c8618180b87f8b71bf6a9ddb8d0c9e89012347b
JeremyIR <jeremy.i.rosenfeld@gmail.com>
2016-05-17 14:57:23 -0700

    update user schema

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,9 +1,4 @@
 import mongoose from 'mongoose';
-<<<<<<< HEAD
-=======
-import relationship from 'mongoose-relationship';
->>>>>>> eec6392555610177ed56504866439e29e99d71c7
-
 const Schema = mongoose.Schema;
 
 export default mongoose.model('User', new Schema({
