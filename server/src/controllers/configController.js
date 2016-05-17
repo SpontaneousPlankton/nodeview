aaa5303303cc7b256c715c88ebded9125e3af20b
JeremyIR <jeremy.i.rosenfeld@gmail.com>
2016-05-16 20:40:58 -0700

    push routers into array

--- server/src/controllers/configController.js
+++ server/src/controllers/configController.js
@@ -5,12 +5,8 @@ export function createOne(request, response) {
     "data.serverType": request.body.data.serverType,
     "data.appName": request.body.data.appName,
     "data.serverSettings.port": request.body.data.serverSettings.port,
-    "data.routers": [{
-      id: request.body.data.routers[0].id,
-      startPoint: request.body.data.routers[0].startPoint,
-      endpoints: request.body.data.routers[0].endpoints,
-      name: request.body.data.routers[0].name,
-    }],
+    '$ push': { 'data.routers': { '$ each': request.body.routers },
+    },
   });
 
   newConfig.save((err) => {
