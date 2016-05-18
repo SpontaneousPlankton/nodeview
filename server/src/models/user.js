2fae1dda294f40fccdac6a60f38a96d44680917b
JeremyIR <jeremy.i.rosenfeld@gmail.com>
2016-05-17 17:25:18 -0700

    update user mode;

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,9 +1,4 @@
 import mongoose from 'mongoose';
-<<<<<<< HEAD
-import relationship from 'mongoose-relationship';
-
-=======
->>>>>>> 93550b124c10db3adbfa030c98326717a318d5f7
 const Schema = mongoose.Schema;
 
 export default mongoose.model('User', new Schema({
